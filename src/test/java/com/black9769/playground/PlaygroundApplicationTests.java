4781fcaa697ad9b99b7e97b5070153c874a5e37e
black9769 <black9769@dnx.kr>
2023-08-10 01:48:23 +0900

    ADD jasypt -ing

--- src/test/java/com/black9769/playground/PlaygroundApplicationTests.java
+++ src/test/java/com/black9769/playground/PlaygroundApplicationTests.java
@@ -1,5 +1,7 @@
 package com.black9769.playground;
 
+import com.black9769.playground.global.config.JasyptConfig;
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -10,4 +12,23 @@ class PlaygroundApplicationTests {
     void contextLoads() {
     }
 
+}
+
+class JasyptTest extends JasyptConfig {
+    @Test
+    public void jasypt_encyrpt_and_decrypt_test(){
+        String text = "jdbc:mariadb://192.168.35.29:3306/test";
+        StandardPBEStringEncryptor jasypt = new StandardPBEStringEncryptor();
+        jasypt.setPassword("password");
+
+        String encryptedText = jasypt.encrypt(text);
+        String decryptedText = jasypt.decrypt(encryptedText);
+
+
+        System.out.println(text);
+        System.out.println(encryptedText);
+        System.out.println(decryptedText);
+
+
+    }
 }
